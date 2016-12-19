b12641e66a23d5c1810eb310a30b41847326a69d
Alexandre Girard <git@alexgirard.com>
2016-12-19 15:01:34 +0100

    make mongoose models compliant with existing parser

--- www/server/models/feedback.js
+++ www/server/models/feedback.js
@@ -2,13 +2,14 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const feedbackSchema = new Schema({
-  product_id: { type: 'String', required: true },
+  prod_id: { type: 'String', required: true },
   user: { type: 'String', required: true },
   delivery_time: { type: 'String', required: true },
   note: { type: 'String', required: true },
   text: { type: 'String', required: true },
   cuid: { type: 'String', required: true },
+  date: { type: 'Date', default: Date.now, required: true },
   dateAdded: { type: 'Date', default: Date.now, required: true },
 });
 
-export default mongoose.model('Feedback', feedbackSchema);
+export default mongoose.model('feedback', feedbackSchema);
--- www/server/models/product.js
+++ www/server/models/product.js
@@ -1,16 +1,16 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
-  website_id: { type: 'String', required: true },
-  name: { type: 'String', required: true },
+const productSchema = new Schema({
+  delivery: { type: 'String', required: true },
+  product: { type: 'String', required: true },
   price_usd: { type: 'String', required: true },
   price_btc: { type: 'String', required: true },
-  vendor_id: { type: 'String', required: true },
+  vendor: { type: 'String', required: true },
+  vendor_url: { type: 'String', required: true },
+  product_id: { type: 'String', required: true },
   product_type: { type: 'String', required: true },
-  delivery: { type: 'String', required: true },
-  cuid: { type: 'String', required: true },
   dateAdded: { type: 'Date', default: Date.now, required: true },
 });
 
-export default mongoose.model('Post', postSchema);
+export default mongoose.model('prod', productSchema);
